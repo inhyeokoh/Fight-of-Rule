eec66d0314d00bebafb5b7bc2cd5a1ca2d3db6f9
carpet <thfcks1539@naver.com>
2023-09-24 17:26:59 +0900

    이동 시스템 80% 정도 완료

--- Assets/MovingAndInputSystem/Scripts/PlayerMovement.cs
+++ Assets/MovingAndInputSystem/Scripts/PlayerMovement.cs
@@ -3,36 +3,102 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+//[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
-    private Vector3 moveDirection;
-    private float moveSpeed = 4f;
+    [SerializeField]
+    private InputAction mouseClickAction;
+    [SerializeField]
+    private float playerSpeed = 10f;
+    [SerializeField]
+    private float rotationSpeed = 3f;
 
+    private Camera mainCamera;
+    private Coroutine coroutine;
+    private Vector3 targetPosition;
 
-    void Start()
+    private CharacterController cc;
+    private Rigidbody rb;
 
+    private int groundLayer;
 
+    public ParticleSystem mouseClickEffect;
+    private void Awake()
     {
-        
+        mainCamera = Camera.main;
+        cc = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
+        groundLayer = LayerMask.NameToLayer("Ground");
+        mouseClickEffect.Stop();
     }
 
-    void Update()
+    private void OnEnable()
     {
-        bool hasControl = (moveDirection != Vector3.zero);
-        if (hasControl)
-        {
-            transform.rotation = Quaternion.LookRotation(moveDirection);
-            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
-        }
+        mouseClickAction.Enable();
+        mouseClickAction.performed += Move;
     }
 
-    public void OnMove(InputValue value)
+    private void OnDisable()
     {
-        Vector2 input = value.Get<Vector2>();
-        if (input != null)
+        mouseClickAction.performed -= Move;
+        mouseClickAction.Disable();
+    }
+
+    private void Move(InputAction.CallbackContext context)
+    {
+        // Layer 처리를 통해 움직일 수 없는 부분을 클릭해서 움직이는 현상을 막아줌.
+        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+        if (Physics.Raycast(ray: ray , hitInfo: out RaycastHit hit) && hit.collider && hit.collider.gameObject.layer.CompareTo(groundLayer)
+            == 0)
         {
-            moveDirection = new Vector3(input.x, 0f, input.y);
-            Debug.Log($"SEND_MESSAGE : {input.magnitude}");
+            // coroutine이 여러 개 동시에 실행되는 것을 막는 if문
+            if (coroutine != null)
+            {
+                StopCoroutine(coroutine);
+            }
+            coroutine = StartCoroutine(PlayerMoveTowards(hit.point));
+            targetPosition = hit.point;
+
+            //파티클 시스템 위치 설정
+            mouseClickEffect.transform.position = hit.point;
+            mouseClickEffect.Play();
         }
+    }
+    
+
+
+    private IEnumerator PlayerMoveTowards(Vector3 target)
+    {
+        float playerDistanceToFloor = transform.position.y - target.y;
+        target.y += playerDistanceToFloor;
+        while (Vector3.Distance(transform.position, target) > 0.1f)
+        {
+            // Ignores Collisions
+            Vector3 destination = Vector3.MoveTowards(transform.position, target, playerSpeed * Time.deltaTime);
+            // transform.position = destination;
+
+            // Character Controller
+            Vector3 direction = target - transform.position;
+            // Vector3 movement = direction.normalized * playerSpeed * Time.deltaTime;
+            // cc.Move(movement);
+
+            // Rigidbody
+            rb.velocity = direction.normalized * playerSpeed;
+
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction.normalized), rotationSpeed * 
+                Time.deltaTime);
+
+            yield return null;
+        }
+        // 목적지에 도착하면 속도를 0으로 바꿈.
+        rb.velocity = Vector3.zero * 0f;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(targetPosition, 1);
     }
 }
