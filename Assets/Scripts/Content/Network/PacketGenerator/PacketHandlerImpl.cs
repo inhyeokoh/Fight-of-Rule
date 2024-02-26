0f28714ed7d84d2e86fa3a130dd198c9ce09a177
inhyeok <oredtiger@naver.com>
2024-02-27 01:17:03 +0900

    PacketHandler base

---
+++ Assets/Scripts/Content/Network/PacketGenerator/PacketHandlerImpl.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacketHandlerImpl : MonoBehaviour
+{
+
+    internal static bool Handle_S_OPTION(Session session, S_OPTION message)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static bool Handle_S_LOGIN(Session session, S_LOGIN message)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static bool Handle_S_NICKNAME(Session session, S_NICKNAME message)
+    {
+        throw new NotImplementedException();
+    }
+}
--- Assets/Scripts/Content/Network/PacketGenerator/__build_result__/PacketHandler.cs
+++ Assets/Scripts/Content/Network/PacketGenerator/__build_result__/PacketHandler.cs
@@ -1,5 +1,4 @@
 using Google.Protobuf;
-using ManagerLib;
 using System;
 using System.Collections.Generic;
 using static PacketHandler;
@@ -24,9 +23,9 @@ class PacketHandler
     Dictionary<ushort, Func<Session, ArraySegment<byte>, bool>> _handler = new Dictionary<ushort, Func<Session, ArraySegment<byte>, bool>>();
     public void Init()
     {
-        _handler.Add((ushort)PacketType.PKT_S_LOGIN, (session, buffer) => PacketHandler_PacketHandler.Handle_S_LOGIN(session, _HandlePacket<S_LOGIN>(buffer)));
-        _handler.Add((ushort)PacketType.PKT_S_OPTION, (session, buffer) => PacketHandler_PacketHandler.Handle_S_OPTION(session, _HandlePacket<S_OPTION>(buffer)));
-        _handler.Add((ushort)PacketType.PKT_S_NICKNAME, (session, buffer) => PacketHandler_PacketHandler.Handle_S_NICKNAME(session, _HandlePacket<S_NICKNAME>(buffer)));
+        _handler.Add((ushort)PacketType.PKT_S_LOGIN, (session, buffer) => PacketHandlerImpl.Handle_S_LOGIN(session, _HandlePacket<S_LOGIN>(buffer)));
+        _handler.Add((ushort)PacketType.PKT_S_OPTION, (session, buffer) => PacketHandlerImpl.Handle_S_OPTION(session, _HandlePacket<S_OPTION>(buffer)));
+        _handler.Add((ushort)PacketType.PKT_S_NICKNAME, (session, buffer) => PacketHandlerImpl.Handle_S_NICKNAME(session, _HandlePacket<S_NICKNAME>(buffer)));
     }
     public ArraySegment<byte> SerializePacket(C_LOGIN pkt) { return _serializePacket(pkt, PacketType.PKT_C_LOGIN); }
     public ArraySegment<byte> SerializePacket(C_OPTION pkt) { return _serializePacket(pkt, PacketType.PKT_C_OPTION); }
